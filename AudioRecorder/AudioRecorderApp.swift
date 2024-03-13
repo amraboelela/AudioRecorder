3201b95f0cc92f19ebdf2559571afb3d5d594216
Amr Aboelela <amraboelela@users.noreply.github.com>
2024-03-13 10:57:01 -0700

    Initial files

---
+++ AudioRecorder/AudioRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRecorderApp.swift
+//  AudioRecorder
+//
+//  Created by Amr Aboelela on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioRecorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioRecorder
+//
+//  Created by Amr Aboelela on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
